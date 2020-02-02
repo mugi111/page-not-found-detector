d3c92be6de12472b5f5a1e78d31d1aad43ca413e
mugi <m.111_t.1230_nittc@outlook.com>
2020-02-02 13:53:49 +0900

    UPDATE: pingの送信

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+const ping = (url) => {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = () =>{};
+
+  xhr.open("GET", url, true);
+  try {
+    xhr.send(null);
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
 var atags = document.getElementsByTagName("a");
 var hrefs = [];
 for (let index = 0; index < atags.length; index++) {
+  href = atags.item(index).href;
+  ping(href);
   hrefs.push(atags.item(index).href);
-}
+};
 
 console.log("hello", hrefs);
